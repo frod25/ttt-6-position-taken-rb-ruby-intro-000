1a61308a2660a85b2cf0dcfe73e5aefa16849722
Felix Rodriguez <frod25@gmail.com>
2020-02-18 19:44:35 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 def position_taken?(board, index)
-  if board[index] == "X" || "O"
-    true
-  else board[index] == " " || ""
+  if board[index] == " " || ""
     false
+  else board[index] == "X" || "O"
+    !false
   end
 end
